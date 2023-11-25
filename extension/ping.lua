06935c278aa07938112896308080a5bb7299e4e5
Marc Balmer <marc.balmer@arcapos.swiss>
2023-11-25 17:38:55 +0100

    Extension get the whole request as table and must return a table

--- extension/ping.lua
+++ extension/ping.lua
@@ -20,10 +20,46 @@
 
 -- The trx-control ping extension
 
-local function ping()
-	return 'pong'
+local config = ... or {}
+
+print 'initializing the trx-control ping extension'
+
+local function vardump(value, depth, key)
+	local linePrefix = ""
+	local spaces = ""
+
+	if key ~= nil then
+		linePrefix = "[" .. key .. "] = "
+	end
+
+	if depth == nil then
+		depth = 0
+	else
+		depth = depth + 1
+		for i = 1, depth do spaces = spaces .. " " end
+	end
+
+	if type(value) == 'table' then
+		local mTable = getmetatable(value)
+		if mTable == nil then
+			print(spaces .. linePrefix .. "(table) ")
+		else
+			print(spaces .. "(metatable) ")
+			value = mTable
+		end
+		for tableKey, tableValue in pairs(value) do
+			vardump(tableValue, depth, tableKey)
+		end
+	elseif type(value) == 'function' or type(value) == 'thread'
+	    or type(value) == 'userdata' or type(value) == nil then
+		print(spaces .. tostring(value))
+	else
+		print(spaces .. linePrefix .. "(" .. type(value) .. ") " ..
+		    tostring(value))
+	end
 end
 
-return {
-	ping = ping
-}
+function ping(request)
+	vardump(request)
+	return { reply = 'pong' }
+end
