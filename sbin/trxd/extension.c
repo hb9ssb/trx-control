4414970dff50180d3ee67563360da1808e87f1d4
Marc Balmer <marc.balmer@arcapos.swiss>
2023-11-25 17:32:14 +0100

    Make generic extensions work

--- sbin/trxd/extension.c
+++ sbin/trxd/extension.c
@@ -43,118 +43,50 @@ extern int verbose;
 static void
 cleanup(void *arg)
 {
-	if (verbose > 1)
-		printf("extension: cleanup\n");
-	lua_close(arg);
+	extension_tag_t *t = (extension_tag_t *)arg;
+
+	lua_close(t->L);
+	free(arg);
 }
 
 void *
 extension(void *arg)
 {
-	extension_tag_t *tag = (extension_tag_t *)arg;
-	lua_State *L;
-	int n, extension_ref;
-	char script[PATH_MAX];
+	extension_tag_t *t = (extension_tag_t *)arg;
 
-	L = NULL;
 	if (pthread_detach(pthread_self()))
 		err(1, "extension: pthread_detach");
-	if (verbose)
-		printf("extension: initialising the %s extension\n", tag->name);
+
+	pthread_cleanup_push(cleanup, arg);
 
 	if (pthread_setname_np(pthread_self(), "trxd-extension"))
 		err(1, "extension: pthread_setname_np");
-	/*
-	 * Lock this transceivers mutex, so that no other thread accesses
-	 * while we are initialising.
-	 */
-	if (pthread_mutex_lock(&tag->mutex))
+
+	if (pthread_mutex_lock(&t->mutex2))
 		err(1, "extension: pthread_mutex_lock");
-	if (verbose > 1)
-		printf("extension: mutex locked\n");
 
-	if (pthread_mutex_lock(&tag->mutex2))
-		err(1, "extension: pthread_mutex_lock");
-	if (verbose > 1)
-		printf("extension: mutex2 locked\n");
-
-	if (strchr(tag->script, '/'))
-		err(1, "extension: script name must not contain slashes");
-
-	/* Setup Lua */
-	L = luaL_newstate();
-	if (L == NULL)
-		err(1, "extension: luaL_newstate");
-
-	pthread_cleanup_push(cleanup, L);
-
-	luaL_openlibs(L);
-
-	snprintf(script, sizeof(script), "%s/%s.lua", _PATH_EXTENSION,
-	    tag->script);
-
-	if (luaL_dofile(L, script))
-		err(1, "extension: %s", lua_tostring(L, -1));
-	if (lua_type(L, -1) != LUA_TTABLE)
-		err(1, "extension: table expected");
-	else
-		extension_ref = luaL_ref(L, LUA_REGISTRYINDEX);
-
-	/*
-	 * We are ready to go, unlock the mutex, so that client-handlers,
-	 * trx-handlers, and, try-pollers can access it.
-	 */
-	if (pthread_mutex_unlock(&tag->mutex))
-		err(1, "extension: pthread_mutex_unlock");
-	if (verbose > 1)
-		printf("extension: mutex unlocked\n");
-
-	while (1) {
-		int nargs = 1;
-
+	for (;;) {
+		t->call = 0;
 		/* Wait on cond, this releases the mutex */
-		if (verbose > 1)
-			printf("extension: wait for cond1\n");
-		while (tag->handler == NULL) {
-			if (pthread_cond_wait(&tag->cond1, &tag->mutex2))
+		while (t->call == 0) {
+			if (pthread_cond_wait(&t->cond1, &t->mutex2))
 				err(1, "extension: pthread_cond_wait");
-			if (verbose > 1)
-				printf("extension: cond1 changed\n");
 		}
-
-		if (verbose > 1) {
-			printf("extension: request for %s", tag->handler);
-			if (tag->data)
-				printf(" with data '%s'\n", tag->data);
-			printf("\n");
+		t->call = 0;
+		switch (lua_pcall(t->L, 1, 1, 0)) {
+		case LUA_OK:
+			break;
+		case LUA_ERRRUN:
+		case LUA_ERRMEM:
+		case LUA_ERRERR:
+			syslog(LOG_ERR, "extension: Lua error: %s",
+			    lua_tostring(t->L, -1));
+			exit(1);
+			break;
 		}
-
-		lua_geti(L, LUA_REGISTRYINDEX, extension_ref);
-		lua_getfield(L, -1, tag->handler);
-		if (lua_type(L, -1) != LUA_TFUNCTION) {
-			tag->reply = "command not supported, "
-			    "please submit a bug report";
-		} else {
-			lua_pushstring(L, tag->data);
-
-			switch (lua_pcall(L, 1, 1, 0)) {
-			case LUA_OK:
-				break;
-			case LUA_ERRRUN:
-			case LUA_ERRMEM:
-			case LUA_ERRERR:
-				syslog(LOG_ERR, "Lua error: %s",
-				    lua_tostring(L, -1));
-				break;
-			}
-		}
-		lua_pop(L, 2);
-		tag->handler = NULL;
-
-		if (pthread_cond_signal(&tag->cond2))
+		t->done = 1;
+		if (pthread_cond_signal(&t->cond2))
 			err(1, "extension: pthread_cond_signal");
-		if (verbose > 1)
-			printf("extension: cond2 signaled\n");
 	}
 	pthread_cleanup_pop(0);
 	return NULL;
