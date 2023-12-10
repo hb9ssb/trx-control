c0903e42ef4d6a5f52dffcfa2bbd13035a010af5
Marc Balmer <marc.balmer@arcapos.swiss>
2023-12-10 11:38:47 +0100

    Add a driver for the ICOM IC-705 transceiver

---
+++ trx/icom-ic-705.lua
@@ -0,0 +1,51 @@
+-- Copyright (c) 2023 Marc Balmer HB9SSB
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to
+-- deal in the Software without restriction, including without limitation the
+-- rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+-- sell copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+-- IN THE SOFTWARE.
+
+-- ICOM IC-705 CI-V driver
+
+local ic705 = require 'ci-v'
+
+ic705.validModes = {
+	['lsb'] = '00',
+	['usb'] = '01',
+	['am'] = '02',
+	['cw'] = '03',
+	['rtty'] = '04',
+	['fm'] = '05',
+	['wfm'] = '06',
+	['cw-r'] = '07',
+	['rtty-r'] = '08',
+	['dv'] = '17'
+}
+
+ic705.filterSetting = {
+	['fil1'] = '01',
+	['fil2'] = '02',
+	['fil3'] = '03'
+}
+
+ic705.name = 'ICOM IC-705'
+
+ic705.frequencyRange = {
+	min = 30000,
+	max = 470000000
+}
+
+return ic705
